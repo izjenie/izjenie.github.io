b8c678fda7a0ec7dca9e6eb2ecb7d3b13b5e94d7
Erlangga Al Farozi <erlangga.al.farozi@gmail.com>
2024-03-28 21:54:34 +0700

    init

--- pages/consul.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <NuxtLayout name="landing">
-      <div>consul</div>
-    </NuxtLayout>
-  </div>
-</template>
-
-<script setup lang="ts"></script>
--- utils/global-type.ts
+++
@@ -1,3 +0,0 @@
-export type DialogTypeEnum = 'suicide' | 'consul'
-
-export type ConsulTabEnum = 'whatsapp' | 'webiste'
